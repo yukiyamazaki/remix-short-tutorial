efdc71fc5e464048310e74df4b13cd643abf4e04
YUKI YAMAZAKI <gooo0523@gmail.com>
2023-06-07 18:27:03 +0900

    feat:end_tutorial

--- app/models/post.server.ts
+++ app/models/post.server.ts
@@ -1,17 +1,17 @@
-type Post = {
-  slug: string;
-  title: string;
-};
+import { prisma } from "~/db.server";
 
-export async function getPosts(): Promise<Array<Post>> {
-  return [
-    {
-      slug: "my-first-post",
-      title: "あああああああああ",
-    },
-    {
-      slug: "90s-mixtape",
-      title: "A Mixtape I Made Just For You",
-    },
-  ];
+import type { Post } from "@prisma/client";
+
+export async function getPosts() {
+  return prisma.post.findMany();
+}
+
+export async function getPost(slug: string) {
+  return prisma.post.findUnique({ where: { slug } });
+}
+
+export async function createPost(
+  post: Pick<Post, "slug" | "title" | "markdown">
+) {
+  return prisma.post.create({ data: post });
 }
